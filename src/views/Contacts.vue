3c1c548cf1a8997d240c8a4fe4713408732f17ab
webCreativeDev <joaoaniceto.webdeveloper@gmail.com>
2022-08-08 16:54:54 +0100

    refactored contacts

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -2,34 +2,35 @@
   <div class="w80">
     <div class="page-title">Contact Us</div>
     <div class="row contacts-comp">
-      <div class="col-6">
+      <div class="col-12 col-xl-6 form">
         <div class="text">
           Feel free to Contact us and we will get book to you as soon as we can.
         </div>
-        <form>
+        <form ref="form" v-on:submit="sendEmail">
           <input
-            v-model="name"
             class="input-type-text"
             placeholder="Name"
+            name="from_name"
             type="text"
           />
           <input
-            v-model="email"
+            name="from_email"
             class="input-type-text"
             placeholder="Email Address"
             type="text"
           />
           <textarea
-            v-model="message"
+
+            name="message"
             class="input-type-message"
             placeholder="Message"
           ></textarea>
           <input class="input-type-button" type="submit" value="Send" />
         </form>
       </div>
-      <div class="col-2 borderRight"></div>
-      <div class="col-2"></div>
-      <div class="col-2">
+      <div class="col-xl-2 borderRight md-none"></div>
+      <div class="col-xl-2 md-none"></div>
+      <div class="col-12 col-xl-2 form">
         <div class="h1-">Contacts</div>
         <div class="text-">
           Parada Lindoso <br />
@@ -38,8 +39,8 @@
         </div>
 
         <div class="h1-">Phone</div>
-        <div class="text-"> 965 627 880</div>
-        <div class="text-"> 965 831 772</div>
+        <div class="text-">965 627 880</div>
+        <div class="text-">965 831 772</div>
 
         <div class="h1-">Email</div>
         <div class="text-">geral@rhcasasdecampo.com</div>
@@ -50,7 +51,29 @@
 </template>
 
 <script>
+import emailjs from '@emailjs/browser';
 import "../assets/styles/contacts.css";
 
-export default {};
+export default {
+  data() {
+    return {
+      serviceID: "service_35wff8h",
+      templateID: "template_zein85a",
+    };
+  },
+  methods: {
+    sendEmail(e) {
+      e.preventDefault();
+      emailjs.init("SZBS_NJRV38Kh0TLy");
+      emailjs.sendForm(this.serviceID, this.templateID,this.$refs.form,"SZBS_NJRV38Kh0TLy").then(
+        () => {
+          alert("Sent!");
+        },
+        (err) => {
+          alert(JSON.stringify(err));
+        }
+      );
+    }
+  },
+};
 </script>

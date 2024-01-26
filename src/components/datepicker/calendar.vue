3d7984668c2b75842dde8a88f14bbeef12ba94c0
Joao Aniceto Rodrigues Rocha <j.aniceto@ext.desigual.com>
2024-01-26 12:59:16 +0000

    refactored build

--- src/components/datepicker/calendar.vue
+++ src/components/datepicker/calendar.vue
@@ -1,14 +1,9 @@
 <script>
 
-import 'v-calendar/dist/style.css';
 
 
 </script>
 
 <template>
-  <v-date-picker 
-  :value="null"
-  color="gray"
-  is-range
-   />
+
 </template>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import VCalendar from 'v-calendar';
 import 'bootstrap';
 
 const app = createApp(App)
 
 app.use(router)
-app.use(VCalendar, {})
 
 
 app.mount('#app')
